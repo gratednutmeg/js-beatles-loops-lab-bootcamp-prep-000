abeb939838ff98881ebaea188766639f1d168e16
Meghan Spencer <emjayess@gmail.com>
2017-08-22 23:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function theBeatlesPlay(musicians,instruments){
   for (let i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  console.log(array);
   return array;
 }
 
