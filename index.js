a1de97577f6d1f08f255127a7309fb2bdb22f973
Meghan Spencer <emjayess@gmail.com>
2017-08-22 23:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ function theBeatlesPlay(musicians,instruments){
 theBeatlesPlay(['John Lennon','Paul McCartney','George Harrison','Ringo Starr'],['Guitar','Bass Guitar','Lead Guitar','Drums']);
 
 function johnLennonFacts() {
-  
+
 }
 
 function iLoveTheBeatles() {
-  
-}
 
+}
